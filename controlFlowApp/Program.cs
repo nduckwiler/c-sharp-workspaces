563f2a4fe92dbae8455f6504c5cedac14ae76ed7
Nick Duckwiler <nduckwiler@gmail.com>
2019-02-14 17:39:46 -0500

    Add control flow app

---
+++ controlFlowApp/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ControlFlowApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = 20;
+
+            if (age >= 35) 
+            {
+                Console.WriteLine("You can be president!");
+            }
+            else if (age >= 21)
+            {
+                Console.WriteLine("You can drink!");
+            }
+            else if (age >= 18)
+            {
+                Console.WriteLine("You can vote!");
+            }
+            else
+            {
+                Console.WriteLine("You can wait!");
+            }
+        }
+    }
+}
