a1a1a8f8cc789fd6f61f330f7bdd37a98574fa62
Nick Duckwiler <nduckwiler@gmail.com>
2019-02-14 17:40:31 -0500

    Add method app

---
+++ methodApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace MethodApp
+{
+    class Program
+    {
+        // Use a static method that returns a string
+        // Write and read from Console
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter a phrase: ");
+            string input = Console.ReadLine();
+            Console.Write("I think you said... ");
+            Console.WriteLine(yell(input));
+        }
+
+        static string yell(string phrase) 
+        {
+            string loudPhrase = phrase.ToUpper() + "!";
+            return loudPhrase;
+        }
+    }
+}
